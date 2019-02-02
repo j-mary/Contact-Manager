e3e6517903cd49c9490b91316376188aba818675
j-mary <ochejejude@gmail.com>
2019-02-02 21:49:22 +0100

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Header(props) {
+  const { brand } = props;
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {brand}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/about" className="nav-link">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+Header.defaultProps = {
+  brand: 'Contact Manager'
+};
+
+export default Header;
