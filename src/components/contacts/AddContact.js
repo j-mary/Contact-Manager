d18593d6dbca838c4f3987cc22a194c93827a35d
j-mary <ochejejude@gmail.com>
2019-02-03 15:09:02 +0100

    Extracted input to it's own component and handled errors

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
 import uuid from 'uuid';
+import TextInputGroup from '../layout/TextInputGroup';
 
 class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    errors: {}
   };
 
   onChange = e => {
@@ -16,6 +18,17 @@ class AddContact extends Component {
   onSubmit = (dispatch, e) => {
     e.preventDefault();
     const { name, email, phone } = this.state;
+
+    // Check for error
+    if (name === '')
+      return this.setState({ errors: { name: 'Name is required' } });
+
+    if (email === '')
+      return this.setState({ errors: { email: 'Email is required' } });
+
+    if (phone === '')
+      return this.setState({ errors: { phone: 'Phone is required' } });
+
     const newContact = {
       id: uuid(),
       name,
@@ -28,12 +41,13 @@ class AddContact extends Component {
     this.setState({
       name: '',
       email: '',
-      phone: ''
+      phone: '',
+      errors: {}
     });
   };
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -45,39 +59,31 @@ class AddContact extends Component {
                 <div className="card-header">Add Contact</div>
                 <div className="card-body">
                   <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                    <div className="form-group">
-                      <label htmlFor="name" />
-                      <input
-                        type="text"
-                        name="name"
-                        className="form-control form-control-lg"
-                        placeholder="Enter Name..."
-                        value={name}
-                        onChange={this.onChange}
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="email" />
-                      <input
-                        type="email"
-                        name="email"
-                        className="form-control form-control-lg"
-                        placeholder="Enter Email..."
-                        value={email}
-                        onChange={this.onChange}
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="phone" />
-                      <input
-                        type="text"
-                        name="phone"
-                        className="form-control form-control-lg"
-                        placeholder="Enter Phone..."
-                        value={phone}
-                        onChange={this.onChange}
-                      />
-                    </div>
+                    <TextInputGroup
+                      label="Name"
+                      name="name"
+                      placeholder="Enter Name..."
+                      value={name}
+                      onChange={this.onChange}
+                      error={errors.name}
+                    />
+                    <TextInputGroup
+                      label="Email"
+                      name="email"
+                      type="email"
+                      placeholder="Enter Email..."
+                      value={email}
+                      onChange={this.onChange}
+                      error={errors.email}
+                    />
+                    <TextInputGroup
+                      label="Phone"
+                      name="phone"
+                      placeholder="Enter Phone..."
+                      value={phone}
+                      onChange={this.onChange}
+                      error={errors.phone}
+                    />
                     <input
                       type="submit"
                       value="Add ontact"
