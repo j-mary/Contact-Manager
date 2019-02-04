25e7e4d81a54a4cee194cd7ca11e757d96146672
j-mary <ochejejude@gmail.com>
2019-02-04 09:56:12 +0100

    Updates

---
+++ precache-manifest.30f7fa2d091dcec43f13e6fde55308c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65dd574dc71532402377",
+    "url": "/Contact-Manager/static/js/runtime~main.65dd574d.js"
+  },
+  {
+    "revision": "9130da7ffb388c64ef9c",
+    "url": "/Contact-Manager/static/js/main.9130da7f.chunk.js"
+  },
+  {
+    "revision": "72b4c0fa310257095982",
+    "url": "/Contact-Manager/static/js/1.72b4c0fa.chunk.js"
+  },
+  {
+    "revision": "9130da7ffb388c64ef9c",
+    "url": "/Contact-Manager/static/css/main.1d6187d9.chunk.css"
+  },
+  {
+    "revision": "72b4c0fa310257095982",
+    "url": "/Contact-Manager/static/css/1.5f3af6dd.chunk.css"
+  },
+  {
+    "revision": "25bdbfdea1a885bf552d63ac16e66b34",
+    "url": "/Contact-Manager/index.html"
+  }
+];
